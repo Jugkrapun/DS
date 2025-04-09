62ce76dd20ff51ee3d7e1c9c0eae50e836764dde
Jugkrapun <jugkrapunbanrai@gmail.com>
2025-04-09 21:19:15 +0700

    save

--- labeling.py
+++ labeling.py
@@ -8,16 +8,16 @@ def label_risk(df):
     Returns:
         pd.DataFrame: Stock data with labeled risk levels.
     """
-    try:
-        df = df.dropna(subset=['Volatility'])  # Ensure no missing values in Volatility
-        quantiles = df['Volatility'].quantile([0.33, 0.66])
-        conditions = [
-            (df['Volatility'] > quantiles[0.66]),
-            (df['Volatility'] <= quantiles[0.66]) & (df['Volatility'] > quantiles[0.33]),
-            (df['Volatility'] <= quantiles[0.33])
-        ]
-        choices = ['High', 'Medium', 'Low']
-        df['Risk Level'] = np.select(conditions, choices)
-        return df
-    except Exception as e:
-        raise ValueError(f"Error during labeling: {e}")
+    # try:
+    #     df = df.dropna(subset=['Volatility'])  # Ensure no missing values in Volatility
+    #     quantiles = df['Volatility'].quantile([0.33, 0.66])
+    #     conditions = [
+    #         (df['Volatility'] > quantiles[0.66]),
+    #         (df['Volatility'] <= quantiles[0.66]) & (df['Volatility'] > quantiles[0.33]),
+    #         (df['Volatility'] <= quantiles[0.33])
+    #     ]
+    #     choices = ['High', 'Medium', 'Low']
+    #     df['Risk Level'] = np.select(conditions, choices)
+    #     return df
+    # except Exception as e:
+    #     raise ValueError(f"Error during labeling: {e}")
